549264e0e19d0a4b72a8de62eb1f3cefc24fccb2
PrikrylovaJ <jprikrylova@email.cz>
2021-04-26 19:49:07 +0200

    HTML pro výběr barvy

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 'use strict';
 
 console.log('funguju');
+
+const oblekElm = document.querySelector('.product__colors');
+oblekElm.innerHTML += `<p>Pomocí tlačítek vyberte barvu produktu</p>
+<div class="colors">
+  <div class="color">
+    <div class="color-box"></div>
+    <p class="color__name">bílá</p>
+  </div>
+</div>
+`
