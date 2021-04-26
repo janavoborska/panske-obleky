86a12452aeff32232b34ab850c3d22c8a3e08619
Jana Voborská <jana.dostalov@seznam.cz>
2021-04-26 19:53:06 +0200

    Ukazka vyberu barev

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 'use strict';
 
 console.log('funguju');
+
+const oblekElm = document.querySelector('.product__colors');
+
+oblekElm.innerHTML = `<p>Vyberte si barvu produktu</p>
+<div class="choose-colors">
+  <div class="color-option">
+    <div class="color-option__box"></div>
+    <p class="color-option__name">bílá</p>
+  </div>
+</div>`;
